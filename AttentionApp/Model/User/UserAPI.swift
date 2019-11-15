17fa53b196c8d5b974237cb9121cce3f8e23c73a
Walter Jean Pierre Huapaya Chavez <jeanpierrehuapaya@gmail.com>
2019-11-14 20:08:36 -0500

    Test

--- AttentionApp/Model/User/UserAPI.swift
+++ AttentionApp/Model/User/UserAPI.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2019 Ricardo Guevara. All rights reserved.
 //
 
+import Alamofire
 import Foundation
+import SwiftyJSON
+
+
+struct UserAPI{
+    
+    static func data(_ parameters: Parameters?, completion: @escaping (Result<JSON>) -> Void){
+        RequestAPI.call("v1/auth/self", method: .get, parameters: parameters){res in
+            switch res{
+                case .success:
+                    if let json = res.value {
+                        completion(.success(json))
+                      }
+                case let .failure(error):
+                  completion(.failure(error))
+            }
+        
+        }
+    }
+    
+}
